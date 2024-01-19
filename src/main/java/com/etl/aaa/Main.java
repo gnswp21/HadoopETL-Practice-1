fab76dba3657682f7e82520b0f30a7ada4a72364
gnswp21 <gnswp21@gmail.com>
2024-01-19 10:12:27 +0900

    install hdfs ok

--- src/main/java/com/etl/aaa/Main.java
+++ src/main/java/com/etl/aaa/Main.java
@@ -1,7 +1,20 @@
 package com.etl.aaa;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.setProperty("HADOOP_CON_DIR", "C:/hadoop-3.3.6/etc/hadoop");
+
+        String HADOOP_CONF_DIR = System.getenv("HADOOP_CONF_DIR");
+
+        Configuration configuration = new Configuration();
+
+        configuration.addResource(new Path("file:///" + HADOOP_CONF_DIR + "/core-site.xml"));
+        configuration.addResource(new Path("file:///" + HADOOP_CONF_DIR + "/hdfs-site.xml"));
+
+        Configuration configuration = new Configuration();
     }
 }
